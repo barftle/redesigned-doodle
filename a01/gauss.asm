71938a9e4485b7a0111edaf03991d7e58d8d3e15
Bruno Stendal <102233520+barftle@users.noreply.github.com>
2022-10-26 11:49:19 +0200

    a01

--- a01/gauss.asm
+++ a01/gauss.asm
@@ -2,12 +2,9 @@
 
 gauss:      mov rax , rdi       ; rax := rdi
             inc rdi
-            mul rsi             ; rdx : rax := rsi * rax
+            mul rdi             ; rdx : rax := rsi * rax
 
-            
-            ; not neccessarily needed here :
-            ; mov rdx , 0
-
-            mov rcx , 2         ; rcx := 2
+            mov rdx , 0
+            mov rcx , 2         ;rcx := 2
             div rcx             ;rax := rax /2 , rdx := rax % 2
             ret
