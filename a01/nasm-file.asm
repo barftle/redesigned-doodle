f8bc06271a10964cb2ecfde8e0f0d0cb57b8993d
Bruno Stendal <102233520+barftle@users.noreply.github.com>
2022-10-26 11:28:14 +0200

    a01

--- a01/nasm-file.asm
+++ a01/nasm-file.asm
@@ -1,3 +1,13 @@
-global tri_area
-tri_area : mov rax , 42
-ret
+            global tri_area
+
+tri_area:   mov rax , rdi       ; rax := rdi
+            inc rdi
+            mul rsi             ; rdx : rax := rsi * rax
+
+            
+            ; not neccessarily needed here :
+            ; mov rdx , 0
+
+            mov rcx , 2         ; rcx := 2
+            div rcx             ;rax := rax /2 , rdx := rax % 2
+            ret
