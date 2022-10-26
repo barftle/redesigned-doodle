8134f6804bcf4ab71ebd93a16b7c9ffa6e21762a
Bruno Stendal <102233520+barftle@users.noreply.github.com>
2022-10-26 12:55:37 +0200

    a01

--- a01/gauss_wrapper.c
+++ a01/gauss_wrapper.c
@@ -24,15 +24,15 @@ int main(int argc, char *argv[])
 	 *  3. the converted value is out of range
 	 */
 	char *endptr;
-	const uint64_t val = strtoull(argv[1], &endptr, 10);
+	const int64_t val = strtoull(argv[1], &endptr, 10);
 	if (argv[1][0] == '\0' || *endptr != '\0' ||
 	    (val == ULONG_MAX && errno == ERANGE)) {
 		fprintf(stderr, "Invalid Argument: \"%s\"\n", argv[1]);
 		return 2;
 	}
 
-	const uint64_t res = gauss(val);
-	const uint64_t chk = gauss_check(val);
+	const int64_t res = gauss(val);
+	const int64_t chk = gauss_check(val);
 	printf("gauss(%" PRIu64 ") = %" PRIu64 "[%s]\n", val, res,
 			chk == res ? "CORRECT" : "WRONG");
 
