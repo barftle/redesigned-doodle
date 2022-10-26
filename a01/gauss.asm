1e652f90c603c97149ea4de6f4c4657bfb5c3b3d
Bruno Stendal <102233520+barftle@users.noreply.github.com>
2022-10-26 11:58:13 +0200

    a01

--- a01/gauss.asm
+++ a01/gauss.asm
@@ -1,8 +1,8 @@
             global gauss
 
 gauss:      mov rax , rdi       ; rax := rdi
-            inc rdi
-            mul rdi             ; rdx : rax := rsi * rax
+            mul rax
+            add rax rdi             ; rdx : rax := rsi * rax
 
             mov rdx , 0
             mov rcx , 2         ;rcx := 2
