b8bae790bec3eb0879be6769f6bdd5ce4279175b
Antidott <32925597+Antidott@users.noreply.github.com>
2022-11-16 11:01:19 +0100

    a02

--- a02/collatz.asm
+++ a02/collatz.asm
@@ -1,3 +1,4 @@
+;Bruno Stendal, Martin Baer, Lukas Gewinner, Christian Schaefer
 GLOBAL collatz
 
 SECTION .text
@@ -7,19 +8,22 @@ collatz:
 	MOV rcx,0 ; das ist k das am Anfang 0 ist.
   .start:
 	CMP rdi, 1 	; Paramenter 1 wird mit 1 Verglichen.
-	JA .if  	; Wenn Parameter 1 größer 1, dann spring zu .if sonst nicht.
-	MOV rax,rcx  ; rcx wird nach rax getan, damit k ausgegeben wird.
+	JA .if  	; Wenn Parameter 1 größer 1, dann spring 
+				; zu .if sonst nicht.
+	MOV rax,rcx  ; rcx wird nach rax getan, damit k 
+				 ; ausgegeben wird.
 	RET			; rax (k) wird ausgegeben
 
   .if:
 	MOV rax, rdi ; Der erste Parameter tut man auf rax.
 	SHR rax,1 	; Die Zahl in rax wird durch 2 geteilt.
 	JC .else 	; wenn carry Flag kommt (beim teilen von Zahl 
-				; wird ein Bit übertragen, wegen dem Rest, in 
-				; das nächsthöchere Bit) springt zur Funktion _else sonst nicht.
+				; wird ein Bit übertragen, wegen dem Rest,
+				; in das nächsthöchere Bit) springt zur 
+				; Funktion _else sonst nicht.
 	SHR rdi,1 	; rdi wird um zwei geteilt.
 	INC rcx 	; rcx = rcx + 1 (rcx entspricht k)
-	JMP .start 	; springt zurück zur Funktion .start zurück.
+	JMP .start 	; springt zur Funktion .start zurück.
 	
 	
   .else:
@@ -27,7 +31,8 @@ collatz:
 	MOV rax,rdi ; rdi wird nach rax getan.
 	MUL r11 	; rax = rax * r11
 	INC rax 	; rax = rax + 1
-	MOV rdi,rax ; rax wird nach rdi getan damit die Schleife funktioniert.
+	MOV rdi,rax ; rax wird nach rdi getan damit die Schleife 
+				; funktioniert.
 	INC rcx 	; rcx = rcx + 1 (rcx entspricht dem k)
 	JMP .start 	; springt zurück zur Funktion .start.
 	
