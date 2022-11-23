4d1d3ea1348909d253778128577a1be0f2355f69
Bruno Stendal <102233520+barftle@users.noreply.github.com>
2022-11-23 13:49:07 +0100

    a03

--- a03/convert.asm
+++ a03/convert.asm
@@ -1,16 +1,17 @@
 ;Bruno Stendal, Martin Baer, Lukas Gewinner, Christian Schaefer          
             global strToInt
 
-strToInt:   mov rcx 0
-        .loop:
-            mov al , [rdi+rcx]
-            sub rax 48
-            inc rcx
-            cmp [rdi+rcx] 0
-            je .end
-            jne .mult
-        .mult
-            mul rsi
-            jmp .loop
-        .end
-            ret
+strToInt:   
+    mov rcx 0
+  .loop:
+    mov al , [rdi+rcx]
+    sub rax 48
+    inc rcx
+    cmp [rdi+rcx] 0
+    je .end
+    jne .mult
+  .mult
+    mul rsi
+    jmp .loop
+  .end
+    ret
