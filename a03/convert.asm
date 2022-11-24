ffeb8b252c7754dfbc5d11890dacd8b276b34f25
Bruno Stendal <bruno.r3ad@gmail.com>
2022-11-24 23:15:26 +0100

    a03

--- a03/convert.asm
+++ a03/convert.asm
@@ -2,20 +2,20 @@
             global strToInt
 
 strToInt:
-    mov rax, 0;
-    mov rcx, 0;
+    mov rax, 0; output ausnullen
+    mov rcx, 0; counter ausnullen
   .loop:
-    mov rdx, 0;
-    mov dl, [rdi+rcx];
-    sub dl, 48;
-    add al, dl;
-    inc rcx;
-    mov dl, [rdi+rcx]; 
-    cmp dl, 0;
-    je .end;
-    jne .mult;
+    mov rdx, 0; data ausnullen
+    mov dl, [rdi+rcx]; erstes 8bit teilregister von rdi nach 8-bit data register moven
+    sub dl, 48; ascii versatz subtrahieren
+    add al, dl; auf die letzten 8bit von rax addieren
+    inc rcx; versatzcounter erhöhen
+    mov dl, [rdi+rcx]; nächstes 8bit teilregister einlesen
+    cmp dl, 0;  mit 0 vergleichen, ob es der nullterimantor ist
+    je .end;  falls ja springe zum ende
+    jne .mult;  falls nein, multipliziere rax mit der basis
   .mult
-    mul rsi;
+    mul rsi;  rax mit der basis multiplizieren
     jmp .loop;
   .end
     ret
