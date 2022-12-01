7ed7b0145ada7507725e1041c8abd3ea47b2d110
Bruno Stendal <bruno.r3ad@gmail.com>
2022-12-01 23:41:15 +0100

    a04

---
+++ a04/calc.asm
@@ -0,0 +1,10 @@
+;Bruno Stendal, Martin Baer, Lukas Gewinner, Christian Schaefer          
+            global calc_add
+
+calc_add:
+    mov rbx, 0;
+    mov rax, 0;
+    mov rdi, 0;
+    mov rsi, 0;
+    mov rdx, 0;
+    ret
