d5f99bcd977deda9cb1818e3c2a900b0925d340d
Bruno Stendal <bruno.r3ad@gmail.com>
2022-12-15 20:30:46 +0100

    a06

--- a06/formula.asm
+++ a06/formula.asm
@@ -1,5 +1,16 @@
 GLOBAL formula_int, formula_flt
-SECTION .text
+
+SECTION .data;
+
+null:   dq  0.0    ;
+acht:   dq  8.0    ;
+vier:   dq  4.0    ;
+zwei:   dq  2.0    ;
+drei:   dq  3.0    ;
+
+
+
+SECTION .text;
 
 formula_int:
 
@@ -72,6 +83,37 @@ formula_int:
 
 
 formula_flt:
+    ;a = xmm0
+    ;b = xmm1
+    ;c = xmm2
+    ;d = xmm3
+    ;e = xmm4
+    ;f = xmm5
+    ;g = xmm6
+    ;h = xmm7
     ;mov xmm0, xmm1;
     ;https://redirect.cs.umbc.edu/courses/undergraduate/313/fall04/burt_katz/lectures/Lect12/floatingpoint.html
+    addsd xmm0, xmm1;
+    movsd xmm1, xmm0;               xmm1 = a+b
+    movsd xmm0, [rel null];
+    subsd xmm2, xmm3;               xmm2 = c-d
+
+    
+    mulsd xmm4, [rel acht];
+    mulsd xmm5, [rel vier];
+    divsd xmm6, [rel zwei];
+    divsd xmm7, [rel vier];
+
+    mulsd xmm1, xmm2;               xmm1 = (a+b)*(c-d)
+    movsd xmm0, xmm1;
+
+    addsd xmm4, xmm5;
+    subsd xmm4, xmm6;
+    addsd xmm4, xmm7;
+
+    mulsd xmm1, xmm4;
+    divsd xmm1, [rel drei];
+
+    movsd xmm0, xmm1;
+
     ret;
